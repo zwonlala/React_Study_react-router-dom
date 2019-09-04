b6537f81c1e1328e9ddfd89dc0aea4558f42b894
jiwon_lala <s26788761@naver.com>
2019-09-04 21:28:40 +0900

    라우트 이동하기#2_NavLink 컴포넌트:

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home,About } from 'pages';
 import Menu from 'components/Menu';
+import Menu2 from 'components/Menu2';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Menu />
+                <Menu2 />
                 <Route exact path="/" component={Home}/>
                 <Switch>
                     <Route path="/about/:name" component={About}/>  
