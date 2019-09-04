8094830fb03cc793affa14766876ee7beef42f82
jiwon_lala <s26788761@naver.com>
2019-09-04 22:18:00 +0900

    라우트 속의 라우트

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home,About } from 'pages';
+import { Home,About, Posts } from 'pages';
 import Menu from 'components/Menu';
 import Menu2 from 'components/Menu2';
 
@@ -15,6 +15,7 @@ class App extends Component {
                     <Route path="/about/:name" component={About}/>  
                     <Route path="/about" component={About}/>
                 </Switch>
+                <Route path="/posts" component={Posts}/>
             </div>
         );
     }
